03b9e378027233d216b1eae1201e57d0337bfaf1
Arda Kizilcay <hello@ardakizilcay.com>
2024-02-26 16:30:52 -0500

    Updates game header component visual layout and css

--- src/components/game-header.js
+++ src/components/game-header.js
@@ -1,15 +1,26 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
+import * as Style from './game-header.module.css'
 
-const GameHeader = ({ imageCover, imageBackground }) => {
-  const cover = getImage(imageCover)
-  const background = getImage(imageBackground)
+const GameHeader = ({ cover, background }) => {
+  const imageCover = getImage(cover)
+  const imageBackground = getImage(background)
 
   return (
-    <header>
-      <GatsbyImage image={cover} alt="" />
-      <GatsbyImage image={background} alt="" />
-      <h1>The Header</h1>
+    <header className={Style.header}>
+      <GatsbyImage image={imageBackground} alt=""
+      className={Style.background} />
+      <div className={Style.headerContent}>
+        <GatsbyImage image={imageCover} alt=""
+        className={Style.boxArt} />
+        <div>
+          <div className={Style.gameInfoContainer}>
+            <div className={Style.gameDate}>Released Feb 24, 2022</div>
+            <div className={Style.gameTitle}>Elden Ring</div>
+          </div>
+          <div className={Style.availabilityContainer}>Available on</div>
+        </div>
+      </div>
     </header>
   )
 }
