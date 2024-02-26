a3f65383f4613570013d016f013010779ff8ef04
Arda Kizilcay <hello@ardakizilcay.com>
2024-02-26 15:14:10 -0500

    Adds common css file

--- src/pages/game/{gamesJson.title}.js
+++ src/pages/game/{gamesJson.title}.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import GameLayout from "../../components/game-layout"
+import "../../styles/common.css"
 
 function GamePage({ data }) {
   return (
