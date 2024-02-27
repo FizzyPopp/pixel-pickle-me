259c5836e9985cac9738df458f23d381f38a96d3
Arda Kizilcay <hello@ardakizilcay.com>
2024-02-27 17:18:41 -0500

    Adds PS5, XSX and XSS logos and logo react components

---
+++ src/components/logos.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+
+import logoPsFive from "../images/logos/logo-psfive.svg"
+import logoSeriesX from "../images/logos/logo-seriesx.svg"
+import logoSeriesS from "../images/logos/logo-seriess.svg"
+
+export function PsFive({ size=64 }) {
+  return <img 
+    src={logoPsFive} 
+    alt="Playstation 5 logo"
+    height={size} />
+}
+
+export function SeriesX({ size=64 }) {
+  return <img 
+  src={logoSeriesX} 
+  alt="Series X logo"
+  height={size} />
+}
+
+export function SeriesS({ size=64 }) {
+  return <img 
+  src={logoSeriesS} 
+  alt="Series S logo"
+  height={size} />
+}
