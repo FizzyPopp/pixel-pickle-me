5f10c182233e8f484438db103373c248cfcff8a3
Arda Kizilcay <hello@ardakizilcay.com>
2024-03-01 16:46:55 -0500

    Cleans up import on logos

--- src/components/logos.js
+++ src/components/logos.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from "react"
 
 import logoPsFive from "../images/logos/logo-psfive.svg"
 import logoSeriesX from "../images/logos/logo-seriesx.svg"
