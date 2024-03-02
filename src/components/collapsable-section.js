e023ea8f796156e74a985ee88e7eafdbec3b2b94
Arda Kizilcay <hello@ardakizilcay.com>
2024-03-01 19:08:51 -0500

    Implements collapsable section functionality

--- src/components/collapsable-section.js
+++ src/components/collapsable-section.js
@@ -1,7 +1,27 @@
 import * as React from "react"
+import * as Style from "./collapsable-section.module.css"
+
+import { useState } from "react"
+
+import { IoChevronUp, IoChevronDown } from "react-icons/io5"
 
 const CollapsableSection = ({ children, title }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
+  function handleCollapseClick() {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  return (
+    <div>
+      <div className={Style.sectionHeader} onClick={handleCollapseClick}>
+        {title}
+        {isCollapsed && <IoChevronDown/>}
+        {!isCollapsed && <IoChevronUp/>}
+      </div>
+      {!isCollapsed && children}
+    </div>
+  )
 }
 
 export default CollapsableSection
