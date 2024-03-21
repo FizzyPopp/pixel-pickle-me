9bb0c7038d02f36f220386ec7ca3670f145feee7
James Liu <james@standof.fish>
2024-03-21 13:11:16 -0400

    Updates platform section to interact through htmx

--- util/page.mjs
+++ util/page.mjs
@@ -1,6 +1,5 @@
 import * as Path from 'path'
 import * as util from 'util'
-import fastify from 'fastify'
 import { readFile, readdir } from 'fs/promises'
 
 import Handlebars from 'handlebars'
@@ -9,7 +8,11 @@ const Q = (obj, depth = 2) => {
   return util.inspect(obj, { depth: depth, colors: true })
 }
 
-export async function page(pageApi, options) {
+Handlebars.registerHelper('slug', function (context) {
+  return context.toLowerCase().replace(/ /g, '-')
+})
+
+export default async function page(pageApi, options) {
   const {
     baseUrl,
     templatePath,
@@ -21,8 +24,10 @@ export async function page(pageApi, options) {
   const templatesList = await readdir(templatePath)
   const templatesRaw = {}
   const templates = {}
+  const sectionData = {}
+  let targetGameName = ''
 
-  const platformDataList = platformsJSON.PlatformEnum.map((plat) => {
+  sectionData.platforms = platformsJSON.PlatformEnum.map((plat) => {
     return {
       ...plat,
       featureList: platformsJSON.PlatformFeatures[plat.platformID].featureList
@@ -42,7 +47,7 @@ export async function page(pageApi, options) {
   Log.debug(`Templates: ${templatesList}`)
   Log.debug(`Built template:`)
   Log.debug(templates['game-select']({ gamesList: Object.keys(db) }))
-  Log.debug(`platformDataList: ${Q(platformDataList)}`)
+  Log.debug(`sectionData: ${Q(sectionData)}`)
   // Log.info(platformDataList)
 
   pageApi.get(baseUrl + '/game-select', async function handler(request, reply) {
@@ -58,12 +63,62 @@ export async function page(pageApi, options) {
       .send(templates['game-select']({ gamesList: gamesList }))
   })
 
-  pageApi.get(baseUrl + '/htmx/:gameName/:section', (request, reply) => {
+  pageApi.get(baseUrl + '/:gameName/:section', (request, reply) => {
     const { gameName, section } = request.params
 
+    Log.debug(`gameName: ${gameName}`)
+    Log.debug(`section: ${section}`)
+
+    const htmlRender = templates[section]({
+      gameName: gameName,
+      sectionData:
+          customizeSectionDataForGame[section](
+            sectionData[section],
+            db[gameName].data
+          ),
+      })
+
+    Log.debug(`htmlRender: ${htmlRender}`)
     reply
       .code(200)
       .type('text/html')
-      .send("Hello World")
+      .send(htmlRender)
   })
+  pageApi.post(baseUrl + '/game-data', async function handler(request, reply) {
+    Log.debug(`gameName: ${Q(request.body)}`)
+    targetGameName = request.body.gameName
+    Log.debug(`targetGameName: ${Q(db[targetGameName].data)}`)
+    reply
+      .code(200)
+      .type('text/html')
+      .send(templates['game-data']({
+        name: targetGameName,
+        ...db[targetGameName].data,
+      }))
+  })
+}
+
+const customizeSectionDataForGame = {
+  platforms: (sectionData, gameData) => {
+    console.log(gameData)
+    console.log(sectionData)
+    const platforms = sectionData.map((plat) => {
+      console.log(plat)
+      plat.active = gameData.platforms.includes(plat.platformID)
+      if (plat.active) {
+        console.log(gameData.platformFeatures.find((pf) => pf.platformId === plat.platformID).featuresActive)
+        const featuresActive = gameData.platformFeatures.find((pf) => {
+          return pf.platformId === plat.platformID
+        }).featuresActive
+
+        plat.featureList = plat.featureList.map((feature) => {
+          feature.active = featuresActive.includes(feature.name)
+          return feature
+        })
+      }
+      return plat
+    })
+    console.dir(platforms, {depth: 5})
+    return platforms
+  }
 }
