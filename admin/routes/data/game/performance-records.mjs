0a68f19e6f44fb667b301019ce10ce89311f52bc
Arda Kizilcay <hello@ardakizilcay.com>
2024-03-29 16:32:43 -0400

    Removes performance records patch placeholder request

--- admin/routes/data/game/performance-records.mjs
+++ admin/routes/data/game/performance-records.mjs
@@ -1,21 +1,20 @@
-async function routes (fastify, options) {
-  fastify.post('/data/game/:gameName/performance-records/:platform/:rt', 
-  async function handler(request, reply) {
-    const { gameName, platform, rt } = request.params
-    
-  })
+async function routes(fastify, options) {
+  fastify.post('/data/game/:gameName/performance-records/:platform/:rt',
+    async function handler(request, reply) {
+      const { gameName, platform, rt } = request.params
 
-  fastify.patch('/data/game/:gameName/performance-records/:platform/:rt', 
-  async function handler(request, reply) {
-    const { gameName, platform, rt } = request.params
-    
-  })
+      const performanceRecord = {
+        context: {},
+        fps: {},
+        resolution: {}
+      }
+    })
 
-  fastify.delete('/data/game/:gameName/performance-records/:platform/:rt', 
-  async function handler(request, reply) {
-    const { gameName, platform, rt } = request.params
-    
-  })
+  fastify.delete('/data/game/:gameName/performance-records/:platform/:rt',
+    async function handler(request, reply) {
+      const { gameName, platform, rt } = request.params
+
+    })
 }
 
 export default routes;
