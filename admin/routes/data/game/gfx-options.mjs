57dfe6c9bca9dee28dba7e75f43cb1928ecf7658
Arda Kizilcay <hello@ardakizilcay.com>
2024-03-29 16:45:53 -0400

    Adds reply to graphics options post request

--- admin/routes/data/game/gfx-options.mjs
+++ admin/routes/data/game/gfx-options.mjs
@@ -16,6 +16,10 @@ async function routes(fastify, options) {
       options.gamesDb[gameName].data.gfxOptions.push(gfxOption)
 
       options.updateGameFile(gameName)
+
+      reply
+        .code(200)
+        .send(request.body.name + " added to " + gameName)
     })
 
   fastify.post('/data/game/:gameName/gfx-options/value',
