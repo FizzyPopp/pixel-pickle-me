a4faa4cab91eff64b139c697713cf16a2adb2c04
Arda Kizilcay <hello@ardakizilcay.com>
2024-03-29 17:06:45 -0400

    Adds delete performance records request functionality

--- admin/routes/data/game/performance-records.mjs
+++ admin/routes/data/game/performance-records.mjs
@@ -29,10 +29,34 @@ async function routes(fastify, options) {
         .send("Record added to " + gameName)
     })
 
-  fastify.delete('/data/game/:gameName/performance-records/:platform/:rt',
+  fastify.delete('/data/game/:gameName/performance-records/:platformId/:rt',
+    {
+      config: {
+        gameNameExists: true,
+        platformIdValid: true,
+        platformIdExistsPass: true
+      }
+    },
     async function handler(request, reply) {
-      const { gameName, platform, rt } = request.params
+      const { gameName, platformId, rt } = request.params
 
+      const performanceRecordContext = {
+        platform: Number(platformId),
+        rt: (rt === 'true'),
+        gfxOptionsSet: request.body.gfxOptionsSet
+      }
+
+      let targetRecord = options.gamesDb[gameName].data.performanceRecords.find(
+        (element) => element.context == performanceRecordContext)
+
+      options.gamesDb[gameName].data.performanceRecords
+        .splice(options.gamesDb[gameName].data.performanceRecords.indexOf(targetRecord), 1)
+
+      options.updateGameFile(gameName)
+
+      reply
+        .code(200)
+        .send("Record deleted from " + gameName)
     })
 }
 
