8d4c9dfccd3a779fdfbec1bf6395568af0f1f81f
Arda Kizilcay <hello@ardakizilcay.com>
2024-04-05 16:14:06 -0400

    Adds pixel pickle logos and logo components

--- src/components/logos.js
+++ src/components/logos.js
@@ -1,8 +1,32 @@
 import * as React from "react"
 
+import logoPixelPickle from "../images/logos/logo-ppm.svg"
+import logoPixelPickleText from "../images/logos/logo-ppm-text.svg"
+import logoPixelPickleTextMini from "../images/logos/logo-ppm-text-mini.svg"
 import logoPsFive from "../images/logos/logo-psfive.svg"
 import logoSeriesX from "../images/logos/logo-seriesx.svg"
 import logoSeriesS from "../images/logos/logo-seriess.svg"
+
+export function LogoPixelPickle({ size=32 }) {
+  return <img 
+    src={logoPixelPickle} 
+    alt="Pixel Pickle logo"
+    height={size} />
+}
+
+export function LogoPixelPickleText({ size=32 }) {
+  return <img 
+    src={logoPixelPickleText} 
+    alt="Pixel Pickle logo text"
+    height={size} />
+}
+
+export function LogoPixelPickleTextMini({ size=32 }) {
+  return <img 
+    src={logoPixelPickleTextMini} 
+    alt="Pixel Pickle logo text"
+    height={size} />
+}
 
 export function PsFive({ size=64 }) {
   return <img 
