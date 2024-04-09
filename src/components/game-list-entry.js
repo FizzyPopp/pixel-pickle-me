a6fdd314f04bcf9459645e02c30b2169c41bc4b9
James Liu <james@standof.fish>
2024-04-09 16:49:02 -0400

    Adds data-based games list with auto-scaling cards

--- src/components/game-list-entry.js
+++ src/components/game-list-entry.js
@@ -5,16 +5,20 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import { IoLogoPlaystation, IoLogoXbox } from "react-icons/io5";
 
-const GameListEntry = ({ title, image }) => {
+const GameListEntry = ({ title, image, slug, platforms }) => {
   const imageCover = getImage(image)
 
   return (
-    <div className={Style.gameListEntry}>
-      <div className={Style.gameListEntryHeader}>{title}</div>
-      <GatsbyImage image={imageCover} alt="" />
-      <div className={Style.gameListEntryPlatforms}>
-        <IoLogoPlaystation size={22} />
-        <IoLogoXbox size={22} />
+    <div className={Style.gameListEntryMargin}>
+      <div className={Style.gameListEntry}>
+        <a href={`/game/${slug}`}>
+          <div className={Style.gameListEntryHeader}>{title}</div>
+          <GatsbyImage image={imageCover} alt="" />
+          <div className={Style.gameListEntryPlatforms}>
+            {platforms.includes(0) ? <IoLogoPlaystation size={22} /> : ''}
+            {platforms.includes(1) || platforms.includes(2) ? <IoLogoXbox size={22} /> : ''}
+          </div>
+        </a>
       </div>
     </div>
   )
